17199820978ce5c9b6d5fe1c1a05394682c67a7a
ouxu <out_xu@outlook.com>
2018-12-11 21:37:09 +0800

    feat: add quickly copy qr-code

--- quick-copy/README.md
+++ quick-copy/README.md
@@ -4,15 +4,15 @@
 
 举个例子: 手机端需要复制 PC 上文字，又不能用 im 传输，这个时候就可以用这个 `quick-copy`，下面就是生成文字 `quick-copy` 的链接
 
-[https://qc-az3lrvhte.now.sh/?quick-copy](https://qc-az3lrvhte.now.sh/?quick-copy)
+[https://qc-kyqbdy8s4.now.sh/?quick-copy](https://qc-kyqbdy8s4.now.sh?quick-copy)
 
 ![](../doc/img/quick-copy.png)
 
 ## Usage
 
-在 `https://qc-az3lrvhte.now.sh/?` 后添加需要复制的文本即可，如果需要换行空格等，请访问生成站点:
+在 `https://qc-kyqbdy8s4.now.sh/?` 后添加需要复制的文本即可，如果需要换行空格等，请访问生成站点:
 
-[https://qc-az3lrvhte.now.sh/gen.html](https://qc-az3lrvhte.now.sh/gen.html)
+[https://qc-kyqbdy8s4.now.sh/gen.html](https://qc-kyqbdy8s4.now.sh/gen.html)
 
 推荐添加到书签栏
 
---
+++ quick-copy/gen.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+  var $textarea = document.getElementById('J-textarea');
+  var baseUrl = location.origin + '?';
+  var clipboard = new ClipboardJS('#J-footer', {
+    text: function () {
+      return baseUrl + encodeURI($textarea.value);
+    }
+  });
+
+  clipboard.on('success', function (e) {
+    var data = create_qrcode($textarea.value);
+    window.notie.confirm({
+      text: '<p>已复制，二维码如下:<p/></br>' + data
+    });
+  });
+
+  clipboard.on('error', function (e) {
+    window.notie.alert({ type: 'error', text: 'Error!', time: 2 });
+  });
+
+  var create_qrcode = function (text) {
+    qrcode.stringToBytes = qrcode.stringToBytesFuncs['UTF-8'];
+    var qr = qrcode(0, 'M');
+    qr.addData(text, 'Byte');
+    qr.make();
+    return qr.createSvgTag();
+  };
+}
